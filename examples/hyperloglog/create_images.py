7ce3c3eaf4ac554d6fd33b189f0eb36f5aef71b5
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-04-03 22:06:35 +0200

    Improve layout of precision comparison

--- examples/hyperloglog/create_images.py
+++ examples/hyperloglog/create_images.py
@@ -6,11 +6,19 @@ import seaborn as sns
 df = pd.read_csv("precision_comparison")
 df["relative_error"] = abs(df.hyperloglog_count / df.exact_count - 1)
 fig, axes = plt.subplots(math.ceil((15-4)/3.0), 3, figsize=(6, 7))
+sns.set(font_scale=0.9)
 for i in range(4, 15):
     img = sns.boxplot(x='samplesize', y='relative_error',
                       data=df[df.hyperloglog_precision == i],
-                      ax=axes.flat[i-4])
-    axes.flat[i-4].set_title('Precision {}'.format(i))
+                      ax=axes.flat[i-4],
+                      linewidth=0.3)
+    img.tick_params(axis='y', pad=3)
+    img.set_xlabel('')
+    img.set_ylabel('')
+    # everything breaks when I try to use LogFormatter here so we just use this ugly solution
+    img.set_xticklabels(["10e{:d}".format(int(math.log10(int(t.get_text()))))
+                         for t in img.get_xticklabels()])
+    img.set_title('Precision {}'.format(i))
 axes.flat[11].axis("off")
 fig.tight_layout()
 plt.savefig("precision_comparison.pgf")
